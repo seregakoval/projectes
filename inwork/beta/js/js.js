c0c3c80c8a7e4aa3b3cdea9f975503395b2094f1
Sergey Koval <sk3170795@gmail.com>
2016-09-29 18:07:41 +0300

    beta

--- inwork/beta/js/js.js
+++ inwork/beta/js/js.js
@@ -18,7 +18,66 @@ try {
     }
 } catch(err) {
 
-};
+}
+
+/*    ANIMATION-TEXT   */
+
+var index = 0;
+var captionLength = 0;
+var captionOptions = ["This is the first one.", "This is the second.", "This is the third :)"]
+
+// this will make the cursor blink at 400ms per cycle
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity: 0
+    }, 400).animate({
+        opacity: 1
+    }, 400);
+}
+
+// this types the caption
+function type() {
+    $caption.html(caption.substr(0, captionLength++));
+    if(captionLength < caption.length+1) {
+        setTimeout('type()', 70);
+    }
+}
+
+// this erases the caption
+function erase() {
+    $caption.html(caption.substr(0, captionLength--));
+    if(captionLength >= 0) {
+        setTimeout('erase()', 50);
+    }
+}
+
+// this instigates the cycle of typing the captions
+function showCaptions() {
+    caption = captionOptions[index];
+    type();
+    if (index < (captionOptions.length - 1)) {
+        index++
+        setTimeout('erase()', 4000);
+        setTimeout('showCaptions()', 6000)
+    } else {
+        setTimeout(function(){
+            $('#cursor').remove()
+        }, 1500)
+    }
+}
+
+
+$(document).ready(function(){
+    // use setInterval so that it will repeat itself
+    setInterval('cursorAnimation()', 600);
+    $caption = $('#caption');
+
+    // use setTimeout so that it only gets called once
+    setTimeout('showCaptions()', 1000);
+});
+
+/*---AND ANIMATION TEXT---*/
+
 
 /*validate_name*/
 errorName = false;
