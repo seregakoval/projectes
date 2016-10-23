65fc3ba6fbeb93e40c66ab88c20169275a4f6a71
koval <kovalsergey09@mail.ru>
2016-10-23 20:37:50 +0300

    beta

--- inwork/beta/js/js.js
+++ inwork/beta/js/js.js
@@ -98,45 +98,45 @@ function validateEmail(){
 }
 $(document).ready(function(){
     
-    (function() {
-
-        var bodyEl = document.body,
-            content = document.querySelector( '.content-wrap' ),
-            openbtn = document.getElementById( 'open-button' ),
-            closebtn = document.getElementById( 'close-button' ),
-            isOpen = false;
-
-        function init() {
-            initEvents();
-        }
-
-        function initEvents() {
-            openbtn.addEventListener( 'click', toggleMenu );
-            if( closebtn ) {
-                closebtn.addEventListener( 'click', toggleMenu );
-            }
-
-            // close the menu element if the target it´s not the menu element or one of its descendants..
-            content.addEventListener( 'click', function(ev) {
-                var target = ev.target;
-                if( isOpen && target !== openbtn ) {
-                    toggleMenu();
-                }
-            } );
-        }
-        function toggleMenu() {
-            if( isOpen ) {
-                classie.remove( bodyEl, 'show-menu' );
-            }
-            else {
-                classie.add( bodyEl, 'show-menu' );
-            }
-            isOpen = !isOpen;
-        }
-
-        init();
-
-    })();
+    // (function() {
+    //
+    //     var bodyEl = document.body,
+    //         content = document.querySelector( '.content-wrap' ),
+    //         openbtn = document.getElementById( 'open-button' ),
+    //         closebtn = document.getElementById( 'close-button' ),
+    //         isOpen = false;
+    //
+    //     function init() {
+    //         initEvents();
+    //     }
+    //
+    //     function initEvents() {
+    //         openbtn.addEventListener( 'click', toggleMenu );
+    //         if( closebtn ) {
+    //             closebtn.addEventListener( 'click', toggleMenu );
+    //         }
+    //
+    //         // close the menu element if the target it´s not the menu element or one of its descendants..
+    //         content.addEventListener( 'click', function(ev) {
+    //             var target = ev.target;
+    //             if( isOpen && target !== openbtn ) {
+    //                 toggleMenu();
+    //             }
+    //         } );
+    //     }
+    //     function toggleMenu() {
+    //         if( isOpen ) {
+    //             classie.remove( bodyEl, 'show-menu' );
+    //         }
+    //         else {
+    //             classie.add( bodyEl, 'show-menu' );
+    //         }
+    //         isOpen = !isOpen;
+    //     }
+    //
+    //     init();
+    //
+    // })();
 
     if($(".input-field input").val() != ""){
         $(".lab1").css({"top":"-17px","font-size":"12px"});
