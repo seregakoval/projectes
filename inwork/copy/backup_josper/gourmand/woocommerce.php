ba5a30f8850e690185309e1dfb993e32d45a8e78
Sergey Koval <sk3170795@gmail.com>
2016-10-31 17:54:12 +0200

    sceal

--- inwork/copy/backup_josper/gourmand/woocommerce.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-    <div class="wrapper-category clearfix">
-       
-        <?php get_sidebar(); ?>
-        <div class="right-tovar woo">
-            <?php woocommerce_content(); ?>
-        </div>
-    </div>
-<?php get_footer(); ?>
