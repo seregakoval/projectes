f88b2a2507cdbeaae0951c62133f9a6f9fc3c632
koval <kovalsergey09@mail.ru>
2016-11-25 22:45:59 +0200

    task

--- inwork/beta/js/js.js
+++ inwork/beta/js/js.js
@@ -17,88 +17,8 @@ new WOW().init();
 //
 // }
 // ------------- VARIABLES ------------- //
-var ticking = false;
-var isFirefox = (/Firefox/i.test(navigator.userAgent));
-var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
-var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive)
-var slideDurationSetting = 600; //Amount of time for which slide is "locked"
-var currentSlideNumber = 0;
-var totalSlideNumber = $(".background").length;
 
-// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
-function parallaxScroll(evt) {
-    if (isFirefox) {
-        //Set delta for Firefox
-        delta = evt.detail * (-120);
-    } else if (isIe) {
-        //Set delta for IE
-        delta = -evt.deltaY;
-    } else {
-        //Set delta for all other browsers
-        delta = evt.wheelDelta;
-    }
 
-    if (ticking != true) {
-        if (delta <= -scrollSensitivitySetting) {
-            //Down scroll
-            ticking = true;
-            if (currentSlideNumber !== totalSlideNumber - 1) {
-                currentSlideNumber++;
-                nextItem();
-            }
-            slideDurationTimeout(slideDurationSetting);
-        }
-        if (delta >= scrollSensitivitySetting) {
-            //Up scroll
-            ticking = true;
-            if (currentSlideNumber !== 0) {
-                currentSlideNumber--;
-            }
-            previousItem();
-            slideDurationTimeout(slideDurationSetting);
-        }
-    }
-}
-
-// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
-function slideDurationTimeout(slideDuration) {
-    setTimeout(function() {
-        ticking = false;
-    }, slideDuration);
-}
-
-// ------------- ADD EVENT LISTENER ------------- //
-var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
-window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
-
-// ------------- SLIDE MOTION ------------- //
-function nextItem() {
-    var $previousSlide = $(".background").eq(currentSlideNumber - 1);
-    $previousSlide.removeClass("up-scroll").addClass("down-scroll");
-    $('.header').addClass('animated fadeInDown').css({"box-shadow":"0 0 1px 2px rgba(0,0,0,0.25)","background":"#fff",'position':"fixed"});
-    $('.left-bg').css({"display":"block"});
-    $(".header-b").addClass('header-fix');
-    $(".menu-button span").css({"background":"#EE4A34"});
-    $(".nav-container ul li a i").css({'color':"#333"});
-    setTimeout(classServises, 300);
-}
-function classServises() {
-    $(".servises-animation").removeClass("slideOutDown");
-    $(".servises-animation").addClass("slideInUp");
-}
-function removeClassServises() {
-    $(".servises-animation").addClass("slideOutDown");
-}
-function previousItem() {
-    var height = $(".background.scroll").scrollTop();
-    if(height == 0 ) {
-        var $currentSlide = $(".background").eq(currentSlideNumber);
-        $currentSlide.removeClass("down-scroll").addClass("up-scroll");
-        removeClassServises();
-    }
-    $('.header').removeClass('animated fadeInDown');
-
-}
 
 /*    ANIMATION-TEXT   */
 var index = 0;
@@ -303,8 +223,10 @@ $(document).ready(function(){
             }
         });
     });
-    $('[data-goto]').click(function() {
-    });
+    var widthWindow = $(window).width();
+    if(widthWindow <= 991) {
+        $(".servises-animation").removeClass("animated");
+    }
 });
 $(document).on('opened', '.remodal', function () {
 
